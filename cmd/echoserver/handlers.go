6f2a21b6408e900af56dc38f855b6b00e381c33b
ricoberger <mail@ricoberger.de>
2025-03-06 22:02:08 +0100

    Add `/request` Handler
    The new `/request` handler can be used to get the request of another
    server. For that a post request can be sent against the `/request`
    endpoint with a bodz which contains the method, url, body and headers of
    the request which should be executed against the target server, e.g.
    
    ```
    curl -vvv -X POST -d '{"method": "POST", "url": "http://localhost:8080/", "body": "test", "headers": {"x-test": "test"}}' http://localhost:8080/request
    ```

--- cmd/echoserver/handlers.go
+++ cmd/echoserver/handlers.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log/slog"
 	"math/big"
 	"net/http"
+	"net/http/httptrace"
 	"net/http/httputil"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/go-chi/render"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/codes"
 )
@@ -151,4 +157,74 @@ func headerSizeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("X-Header-Size", strings.Repeat("0", size))
 	render.Status(r, http.StatusOK)
 	render.Data(w, r, []byte(http.StatusText(http.StatusOK)))
+}
+
+var httpClient = &http.Client{
+	Transport: otelhttp.NewTransport(
+		http.DefaultTransport,
+		otelhttp.WithClientTrace(func(ctx context.Context) *httptrace.ClientTrace {
+			return otelhttptrace.NewClientTrace(ctx, otelhttptrace.WithoutSubSpans())
+		}),
+	),
+}
+
+type Request struct {
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Body    string            `json:"body"`
+	Headers map[string]string `json:"headers"`
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, span := handlerTracer.Start(r.Context(), "requestHandler")
+	defer span.End()
+
+	var request Request
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		slog.ErrorContext(ctx, "Failed to decode request body.", slog.Any("error", err))
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req, err := http.NewRequestWithContext(ctx, request.Method, request.URL, strings.NewReader(request.Body))
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to create http request.", slog.Any("error", err))
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for key, value := range request.Headers {
+		req.Header.Add(key, value)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to do http request.", slog.Any("error", err))
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to read reespons body.", slog.Any("error", err))
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	render.Status(r, resp.StatusCode)
+	render.Data(w, r, body)
 }
