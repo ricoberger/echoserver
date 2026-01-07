c9aebb3cf244325ce7723418164d78a2f0f71216
ricoberger <mail@ricoberger.de>
2026-01-07 20:59:06 +0100

    Add Option to Log Resource Attributes
    It is now possible to log resource attributes when the
    `OTEL_LOGS_EXPORTER` environment variable is set to `console`. To log
    the resource attributes, the `LOG_RESOURCE_ATTRIBUTES` environment
    variable must be set to `true`.

--- pkg/instrument/logger.go
+++ pkg/instrument/logger.go
@@ -7,10 +7,11 @@ import (
 
 	"github.com/ricoberger/echoserver/pkg/httpserver/middleware/requestid"
 
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/trace"
 )
 
-func setupConsoleLogger() *slog.Logger {
+func setupConsoleLogger(defaultResource *resource.Resource) *slog.Logger {
 	var handler slog.Handler
 
 	var level slog.Level
@@ -32,7 +33,14 @@ func setupConsoleLogger() *slog.Logger {
 		})
 	}
 
-	handler = &CustomHandler{handler}
+	// If the LOG_RESOURCE_ATTRIBUTES environment variable is set to true,
+	// we add the resource attributes to each log record.
+	if os.Getenv("LOG_RESOURCE_ATTRIBUTES") == "true" {
+		handler = &CustomHandler{handler, defaultResource}
+	} else {
+		handler = &CustomHandler{handler, nil}
+	}
+
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
 
@@ -41,6 +49,7 @@ func setupConsoleLogger() *slog.Logger {
 
 type CustomHandler struct {
 	slog.Handler
+	*resource.Resource
 }
 
 func (h *CustomHandler) Handle(ctx context.Context, r slog.Record) error {
@@ -53,6 +62,12 @@ func (h *CustomHandler) Handle(ctx context.Context, r slog.Record) error {
 		r.Add("trace_id", slog.StringValue(span.TraceID().String()))
 		r.Add("trace_flags", slog.StringValue(span.TraceFlags().String()))
 		r.Add("span_id", slog.StringValue(span.SpanID().String()))
+	}
+
+	if h.Resource != nil {
+		for _, attr := range h.Attributes() {
+			r.Add(string(attr.Key), slog.StringValue(attr.Value.AsString()))
+		}
 	}
 
 	return h.Handler.Handle(ctx, r)
